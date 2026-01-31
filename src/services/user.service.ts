6b42691b699237c5db720f74cfb37d3f1e95898e
Dev-Maidu <maidulislammanik8991@gmail.com>
2026-02-01 02:01:32 +0800

    implement role based access with proxy

--- src/services/user.service.ts
+++ src/services/user.service.ts
@@ -1,25 +1,32 @@
 import { env } from "@/env";
 import { cookies } from "next/headers";
+
 const AUTH_URL = env.AUTH_URL;
+
 export const userService = {
   getSession: async function () {
     try {
       const cookieStore = await cookies();
-      //   console.log(cookieStore.toString());
+
+      console.log(cookieStore.toString());
+
       const res = await fetch(`${AUTH_URL}/get-session`, {
         headers: {
           Cookie: cookieStore.toString(),
         },
         cache: "no-store",
       });
+
       const session = await res.json();
-      if (session.data == null) {
-        return { data: null, error: { message: "Session is missing" } };
+
+      if (session === null) {
+        return { data: null, error: { message: "Session is missing." } };
       }
+
       return { data: session, error: null };
-    } catch (error) {
-      console.log(error);
-      return { data: null, error: { message: "Something went wrong" } };
+    } catch (err) {
+      console.error(err);
+      return { data: null, error: { message: "Something Went Wrong" } };
     }
   },
 };
