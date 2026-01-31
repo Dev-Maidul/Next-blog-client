d876d78b1fd44e5cf03baeef25d429d732281a3d
Dev-Maidu <maidulislammanik8991@gmail.com>
2026-02-01 00:46:19 +0800

    create proxy file

--- src/services/user.service.ts
+++ src/services/user.service.ts
@@ -1,11 +1,12 @@
+import { env } from "@/env";
 import { cookies } from "next/headers";
-
+const AUTH_URL = env.AUTH_URL;
 export const userService = {
   getSession: async function () {
     try {
       const cookieStore = await cookies();
       //   console.log(cookieStore.toString());
-      const res = await fetch("http://localhost:4000/apu/auth/get-session", {
+      const res = await fetch(`${AUTH_URL}/get-session`, {
         headers: {
           Cookie: cookieStore.toString(),
         },
