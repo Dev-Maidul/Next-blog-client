a85f57fe6f194b0825b3b296be0f8e32aa5863fe
Dev-Maidu <maidulislammanik8991@gmail.com>
2026-02-01 20:16:50 +0800

    implent isr for data fetching

--- src/app/(commonLayout)/page.tsx
+++ src/app/(commonLayout)/page.tsx
@@ -1,12 +1,15 @@
-import { Button } from "@/components/ui/button";
+import BlogCard from "@/components/modules/homepage/BlogCard";
 import { blogService } from "@/services/blog.service";
+import { BlogPost } from "@/types";
 
 export default async function Home() {
   const { data } = await blogService.getBlogPosts();
   console.log(data);
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <Button>Shadcn button</Button>
+    <div className="grid grid-cols-4 gap-4 max-w-10/12 mx-auto py-4">
+      {data?.data?.map((post: BlogPost) => (
+        <BlogCard key={post.id} post={post}></BlogCard>
+      ))}
     </div>
   );
 }
--- src/services/blog.service.ts
+++ src/services/blog.service.ts
@@ -1,10 +1,12 @@
 import { env } from "@/env";
-
+//* No Dynamic and No { cache: no-store } : SSG -> Static Page
+//* { cache: no-store } : SSR -> Dynamic Page
+//* next: { revalidate: 10 } : ISR -> Mix between static and dynamic
 const API_URL = env.API_URL;
 export const blogService = {
   getBlogPosts: async function () {
     try {
-      const res = await fetch(`${API_URL}/posts`);
+      const res = await fetch(`${API_URL}/posts`, { next: { revalidate: 10 } });
       const data = await res.json();
       return { data: data, error: null };
     } catch (error) {
