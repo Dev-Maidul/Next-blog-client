a1dfb82179d8fbf8c96ec9a5f7680b9419ec3582
Dev-Maidu <maidulislammanik8991@gmail.com>
2026-02-02 16:08:12 +0800

    implement dynamic route segment

--- src/app/(commonLayout)/page.tsx
+++ src/app/(commonLayout)/page.tsx
@@ -3,8 +3,16 @@ import { blogService } from "@/services/blog.service";
 import { BlogPost } from "@/types";
 
 export default async function Home() {
-  const { data } = await blogService.getBlogPosts();
-  console.log(data);
+  const { data } = await blogService.getBlogPosts(
+    {
+      isFeatured: false,
+    },
+    {
+      cache: "no-store",
+    },
+  );
+
+  // console.log(data);
   return (
     <div className="grid grid-cols-4 gap-4 max-w-10/12 mx-auto py-4">
       {data?.data?.map((post: BlogPost) => (
--- src/services/blog.service.ts
+++ src/services/blog.service.ts
@@ -1,16 +1,59 @@
 import { env } from "@/env";
+
+const API_URL = env.API_URL;
+
 //* No Dynamic and No { cache: no-store } : SSG -> Static Page
 //* { cache: no-store } : SSR -> Dynamic Page
 //* next: { revalidate: 10 } : ISR -> Mix between static and dynamic
-const API_URL = env.API_URL;
+
+interface ServiceOptions {
+  cache?: RequestCache;
+  revalidate?: number;
+}
+
+interface GetBlogsParams {
+  isFeatured?: boolean;
+  search?: string;
+}
+
 export const blogService = {
-  getBlogPosts: async function () {
+  getBlogPosts: async function (
+    params?: GetBlogsParams,
+    options?: ServiceOptions,
+  ) {
     try {
-      const res = await fetch(`${API_URL}/posts`, { next: { revalidate: 10 } });
+      const url = new URL(`${API_URL}/posts`);
+
+      if (params) {
+        Object.entries(params).forEach(([key, value]) => {
+          if (value !== undefined && value !== null && value !== "") {
+            url.searchParams.append(key, value);
+          }
+        });
+      }
+
+      const config: RequestInit = {};
+
+      if (options?.cache) {
+        config.cache = options.cache;
+      }
+
+      if (options?.revalidate) {
+        config.next = { revalidate: options.revalidate };
+      }
+
+      const res = await fetch(url.toString(), config);
+
       const data = await res.json();
+
+      // This is an example
+      //   if(data.success) {
+      //     return
+      //   }
+
       return { data: data, error: null };
-    } catch (error) {
-      return { data: null, error: { message: "Something went wrong" } };
+    } catch (err) {
+      return { data: null, error: { message: "Something Went Wrong" } };
     }
   },
 };
