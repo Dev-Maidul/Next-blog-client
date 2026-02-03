121e4d29e67ee914776dcbf61ed7ed545da8e310
Dev-Maidu <maidulislammanik8991@gmail.com>
2026-02-04 02:35:14 +0800

    create register form and connect with tanstack form with zod validation

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,7 @@
 import { defineConfig, globalIgnores } from "eslint/config";
 import nextVitals from "eslint-config-next/core-web-vitals";
 import nextTs from "eslint-config-next/typescript";
+import { Roles } from "./src/constants/roles";
 
 const eslintConfig = defineConfig([
   ...nextVitals,
@@ -13,6 +14,16 @@ const eslintConfig = defineConfig([
     "build/**",
     "next-env.d.ts",
   ]),
+  {
+    Roles: {
+      "react/no-children-prop": [
+        true,
+        {
+          allowFunctions: true,
+        },
+      ],
+    },
+  },
 ]);
 
 export default eslintConfig;
